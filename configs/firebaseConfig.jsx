0b7a7729f27213fa6e981b631361a2583f34c7a2
Siddharth Singh <sid018singh@gmail.com>
2025-03-16 21:38:05 +0530

    final3

--- configs/firebaseConfig.jsx
+++ configs/firebaseConfig.jsx
@@ -7,13 +7,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyAAYEgP08WpM05UP8DU1ACkPlS_H3mul0o",
+  authDomain: "ai-video-generator-c832d.firebaseapp.com",
+  projectId: "ai-video-generator-c832d",
+  storageBucket: "ai-video-generator-c832d.firebasestorage.app",
+  messagingSenderId: "99623955857",
+  appId: "1:399623955857:web:f30e54629d3759f5bd6630",
+  measurementId: "G-Z8PH1CYDN5"
 };
 
 // Initialize Firebase
