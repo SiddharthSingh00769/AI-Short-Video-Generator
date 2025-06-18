436bb533ba7100e8b467dfc3f24750077f325b7b
Siddharth Singh <144914078+SiddharthSingh00769@users.noreply.github.com>
2025-06-18 16:57:51 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Header from "./_components/Header";
 import Hero from "./_components/Hero";
 export default function Home() {
   return (
-    <div className="md:px-16 lg:px-24 xl:px-36">
+    <div className="md:px-16 lg:px-24 xl:px-36">//
       <Header />
 
       <Hero />
